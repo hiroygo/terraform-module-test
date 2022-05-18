ce94cfcd88247f3790e84e4c0469eb31a08b73d1
hiroygo <pcxn1a@gmail.com>
2022-05-18 10:45:38 +0900

    add terraform module

---
+++ foo/terraform/main.tf
@@ -0,0 +1,19 @@
+variable "name" {}
+
+resource "aws_iam_role" "this" {
+  name = var.name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
